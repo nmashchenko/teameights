b93a86c2349942ba12b7e645ac56ac6708031cf7
Leland Moy <Leland.d.moy@gmail.com>
2022-11-16 10:04:31 -0600

    Update ProfileForm.js

--- client/src/components/ProfileForm/ProfileForm.js
+++ client/src/components/ProfileForm/ProfileForm.js
@@ -2,7 +2,57 @@ import React from 'react'
 
 export const ProfileForm = () => {
   return (
-    <div>ProfileForm</div>
+    // right profile box
+    <div>
+      
+      <div>
+        <img />
+      </div>
+
+      <div>
+        <h3>
+          name
+        </h3>
+        <p>
+          title
+        </p>
+      </div>
+
+      <line></line>
+
+      <div>
+        <p>
+          Location
+        </p>
+        <p>
+          Experience
+        </p>
+        <p>
+          Email
+        </p>
+        <p>
+          github
+        </p>
+        <p>
+          linkedin
+        </p>
+      </div>
+
+    
+      
+    </div>
+
+    //
+    // left info box
+    //
+
+    // lanaguage box
+
+    // Tools box
+
+    // Team box
+
+    // About me box
   )
 }
 
