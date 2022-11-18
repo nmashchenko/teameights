6e6f619379525a77fcdb27dab813a9d750f274b6
Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>
2022-11-18 15:59:32 -0600

    Some code style changes

--- client/src/components/Forms/NoTeamForm/NoTeamForm.js
+++ client/src/components/Forms/NoTeamForm/NoTeamForm.js
@@ -1,45 +1,53 @@
-import { 
-    Container,
-    TextContainer,
-    Text, Text2, Text3,
-    ButtonGeneral, ButtonGeneral2,
-    Container2, ToolbarContainer, NavContainer, NavContainer2, NavContainer3, LogoText
+import {
+  Container,
+  TextContainer,
+  Text,
+  ButtonGeneral,
+  Container2,
+  ToolbarContainer,
+  NavContainer,
+  NavContainer3,
+  ButtonContainer,
+  LogoText,
+  CardContainer,
+  Card,
 } from './NoTeamForm.styles'
 
+import TeameightsLogo from '../../../assets/Team/TeameightsLogo.js'
 import NotificationLogo from '../../../assets/Sidebar/Notification'
 import ExitLogo from '../../../assets/Sidebar/Exit'
 import Sidebar from '../../../assets/NavBarIcon'
 
-function NoTeamForm()  {
-    return (
-        <Container>
-            <ToolbarContainer>
-                <NavContainer>
-                    <span class="mousechange"><Sidebar /></span>
-                    <Text2>My Team</Text2>
-                </NavContainer>
-                
-                <LogoText>team8s</LogoText>
-                <NavContainer2>
-                    <NavContainer3>
-                        <ExitLogo />
-                        <Text3>Exit</Text3>
-                    </NavContainer3>
-                    <span class="mousechange"><NotificationLogo /></span>
-                </NavContainer2>
-            </ToolbarContainer>
-            <Container2>
-                <TextContainer>
-                    <Text>You don't have a team yet!</Text>
-                    <Text>Let's create it...</Text>
-                </TextContainer>
-                <buttonContainer>
-                    <ButtonGeneral><span>Create team</span></ButtonGeneral>
-                    <ButtonGeneral2><span>Join existing</span></ButtonGeneral2>
-                </buttonContainer>
-            </Container2>
-        </Container>
-    )
-    }
+function NoTeamForm() {
+  const handleClick = () => {
+    console.log('hello')
+  }
+
+  return (
+    <Container>
+      <ToolbarContainer>
+        <NavContainer>
+          <Sidebar />
+          <Text fontSize="18px" fontWeight="500">
+            My Team
+          </Text>
+        </NavContainer>
+        <TeameightsLogo />
+      </ToolbarContainer>
+      <CardContainer>
+        <Card>
+          <TextContainer>
+            <Text>You don't have a team yet!</Text>
+            <Text>Let's create it...</Text>
+          </TextContainer>
+          <ButtonContainer>
+            <ButtonGeneral onClick={handleClick}>Create team</ButtonGeneral>
+            <ButtonGeneral>Join existing</ButtonGeneral>
+          </ButtonContainer>
+        </Card>
+      </CardContainer>
+    </Container>
+  )
+}
 
 export default NoTeamForm
--- client/src/components/Forms/NoTeamForm/NoTeamForm.styles.js
+++ client/src/components/Forms/NoTeamForm/NoTeamForm.styles.js
@@ -2,169 +2,106 @@ import styled from 'styled-components'
 import { WHITE, BLACK, GREEN } from '../../../constants/colors'
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background: ${BLACK.background};
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  background: ${BLACK.background};
 `
 
-export const ToolbarContainer = styled.header`
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 78px;
-    position: fixed;
-    z-index: 100;
-    background: ${BLACK.background};
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
+export const ToolbarContainer = styled.div`
+  width: 100%;
+  background: ${BLACK.background};
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  align-items: center;
+  ${
+    '' /* flex-direction: row;
+  justify-content: space-between;
+  align-items: center; */
+  }
+  height: 78px;
 `
 
 export const NavContainer = styled.div`
-    position: fixed;
-    left: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-left: 50px;
-
-    gap: 50px;
-    justify-content: space-start;
-
-    .mousechange:hover {
-        cursor: pointer;
-    }
-`
-
-export const Text2 = styled.h3`
-    font-family: 'NoirPro-Regular';
-    font-size: 16px;
-    color: #FFFFFF;
-
-`
-
-export const LogoText = styled.div`
-    display: flex;
-    font-weight: 700;
-    font-size: 30px;
-    color: ${WHITE.main};
-    cursor: pointer;
-    text-align: center;
-    align-self: center;
-    position: fixed;
-
-`
-
-export const NavContainer2 = styled.div`
-    position: fixed;
-    right: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-right: 50px;
-
-    .mousechange:hover {
-        cursor: pointer;
-      }
-    
-`
-
-export const Text3 = styled.h3`
-
-    font-family: 'NoirPro-Regular';
-    font-size: 16px;
-    text-align: center;
-    color: ${GREEN.text};
-
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  margin-right: 80px;
 `
 
 export const NavContainer3 = styled.div`
-    cursor: pointer;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin-right: 50px;
-    margin-left: 50px;
-    gap: 5px;
-
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
 `
 
-export const Container2 = styled.div`
-    display: flex;
-    flex-direction: column;    
-    justify-content: center;
-    align-items: center;
-
-    position: absolute;
-    width: 570px;
-    height: 370px;
-
-    background: #1A1C22;
-    box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.25);
-    border-radius: 15px;
-    margin: 215px 355px 215px 355px;
+export const CardContainer = styled.div`
+  height: calc(100vh - 78px);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 570px;
+  height: 370px;
+  background: #1a1c22;
+  box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  margin: 215px 355px 215px 355px;
+`
 
 export const TextContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: left;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: left;
 `
 
 export const Text = styled.h3`
-    font-weight: ${(props) => props.fontWeight || '500'};
-    font-size: ${(props) => props.fontSize || '24px'};
-    font-family: 'NoirPro-Regular';
-    margin: ${(props) => props.margin || '0 0 0 0'};
-    color: ${WHITE.main};
-    text-align: left;
+  font-weight: ${(props) => props.fontWeight || '500'};
+  font-size: ${(props) => props.fontSize || '24px'};
+  margin: ${(props) => props.margin || '0 0 0 0'};
+  color: ${(props) => props.color || WHITE.main};
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 25px 0 0px;
+  gap: 30px;
 `
 
 export const ButtonGeneral = styled.button`
-    font-weight: 600;
-    font-size: 16px;
+  font-weight: 600;
+  font-size: 16px;
+  border: none;
+  width: 133px;
+  height: 45px;
+  background: ${GREEN.button};
+  color: ${WHITE.main};
+  text-align: center;
+  border-radius: 10px;
+  box-shadow: 0px 4px 50px rgba(93, 157, 11, 0.15);
+
+  &:hover {
     border: none;
-    width: 133px;
-    height: 45px;
-    background: ${GREEN.button};
-    color: ${WHITE.main};
-    text-align: center;
-    border-radius: 10px;
-    margin-right: 70px;
-    margin-left: 10px;
-    
-
-    &:hover {
-        border: none;
-        cursor: pointer;
-        transition: 0.15s;
-    }
-
+    cursor: pointer;
+    transition: 0.15s;
+  }
 `
-
-export const ButtonGeneral2 = styled.button`
-    font-weight: 600;
-    font-size: 16px;
-    border: none;
-    width: 133px;
-    height: 45px;
-    background: ${GREEN.button};
-    color: ${WHITE.main};
-    text-align: center;
-    border-radius: 10px;
-    margin-top: 50px;
-
-    &:hover {
-        border: none;
-        cursor: pointer;
-        transition: 0.15s;
-    }
-`
-
