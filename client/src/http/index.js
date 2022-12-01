12a06f2d01f2adfdbe1bf0866e160303e9b1a7a8
Nikita Mashchenko <mashchenko1@icloud.com>
2022-11-30 23:52:14 -0600

    Update index.js

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 // * API_URL
-export const API_URL = 'http://localhost:5000/api'
+export const API_URL = 'https://teameights-server.herokuapp.com/api'
 
 const api = axios.create({
   withCredentials: true,
