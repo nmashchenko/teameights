9b38c18de39d04c53ef3a46f8749139db7294dc7
Zach Tepper <zach.tepp@gmail.com>
2022-12-02 23:12:36 -0600

    Added: Submission, Tournament, and Leaderboard models

---
+++ server/models/Leaderboard.js
@@ -0,0 +1,38 @@
+// * Modules
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const modelName = "Leaderboard";
+
+const LeaderboardSchema = new mongoose.Schema(
+  {
+    leaderboard_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    tournament_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Tournament",
+    },
+    results: [
+      {
+        team_id: {
+          type: Schema.types.ObjectId,
+          required: true,
+        },
+        teamTotalScore: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+module.exports = mongoose.model(modelName, LeaderboardSchema, modelName); // * 1st - model name, 2nd - schema name, 3d - collection name
