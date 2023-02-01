27a7fb11e438bd8f6df41878bfd6b7f4b4993438
dserra22 <75829857+dserra22@users.noreply.github.com>
2023-01-31 21:04:16 -0600

    updated comment

--- client/src/screens/UsersList/components/Cards/Cards.js
+++ client/src/screens/UsersList/components/Cards/Cards.js
@@ -54,6 +54,9 @@ const Cards = ({ handleOpen, isLoadingUseData, displayFiltered, setIsNotFound })
   // clickHandler is attached to CardContainer, but we are only allowing clicks in which
   // .closest returns the card figure. (.closest gets the closest parent with the
   // specified argument and returns the DOM element)
+  // since UserCard is an HTML figure, it only allows events within the figure to be picked
+  // OR we can just attach the onClick event handler to UserCard itself and we dont have to
+  // do anything fancy
   const fixPropagation = (e, user) => {
     if (e.target.closest('figure') === null) {
       return
