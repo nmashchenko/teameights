afbadcc053bda028f6543985efb5997b9ba9f02c
MelCode <madowl052@gmail.com>
2023-04-13 18:48:40 +0300

    fix link item styles

--- client/src/components/NavBar/NavItem/NavItem.jsx
+++ client/src/components/NavBar/NavItem/NavItem.jsx
@@ -1,26 +1,4 @@
-import { NavLink } from 'react-router-dom'
-import styled from 'styled-components'
-
-import { IconNav, ItemTitle, StyledNavItem } from './NavItem.styles'
-
-const StyledLink = styled(NavLink)`
-  position: relative;
-  transition: opacity 0.2s;
-  border-radius: 10px;
-  text-decoration: none;
-  color: #fff;
-  padding: 8px 16px;
-  transition: background-color 0.5s;
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  &:hover {
-    background-color: #2f3239;
-  }
-  &.active {
-    background-color: #5d9d0b;
-  }
-`
+import { IconNav, ItemTitle, StyledLink, StyledNavItem } from './NavItem.styles'
 
 const NavItem = ({ active, ...props }) => {
   return (
--- client/src/components/NavBar/NavItem/NavItem.styles.js
+++ client/src/components/NavBar/NavItem/NavItem.styles.js
@@ -1,6 +1,26 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const StyledNavItem = styled.li``
+
+export const StyledLink = styled(NavLink)`
+  position: relative;
+  transition: opacity 0.2s;
+  border-radius: 10px;
+  text-decoration: none;
+  color: #fff;
+  padding: 8px 16px;
+  transition: background-color 0.5s;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  &:hover {
+    background-color: #2f3239;
+  }
+  &.active {
+    background-color: #5d9d0b;
+  }
+`
 
 export const IconNav = styled.div`
   display: flex;
