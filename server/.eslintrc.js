80ee26df8c234bca664c99e730b6e4b97c30c3d3
Nikita Mashchenko <mashchenko1@icloud.com>
2023-04-30 21:39:43 -0500

    Update .eslintrc.js

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,54 +2,31 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir : __dirname, 
     sourceType: 'module',
-    ecmaVersion: 'latest',
-    ecmaFeatures: {
-      jsx: false
-    }
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort', 'sonarjs', 'import', 'unicorn', 'prettier'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:security/recommended',
-    'plugin:unicorn/recommended'
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
-    jest: true
+    jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'webpack-hmr.config.js'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "error",
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    'import/first': 'error',
-    'import/newline-after-import': 'error',
-    'import/no-duplicates': 'error',
-    'prettier/prettier': ['error', {
-      'parser': 'typescript',
-      'singleQuote': true,
-      'tabWidth': 2,
-      'tabs': false,
-      'semicolons': true,
-      'arrowParens': 'avoid',
-      'trailingComma': 'all'
-    }],
-    'unicorn/prevent-abbreviations': 'off',
-    'unicorn/filename-case': 'off',
-    'unicorn/no-null': 'off',
-    'unicorn/prefer-module': 'off',
-    'unicorn/prefer-top-level-await': 'off',
-    'sonarjs/no-duplicate-string': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'unicorn/no-array-method-this-argument': 'off',
-    'unicorn/no-array-callback-reference': 'off'
-  }
+  },
+  "prettier/prettier": [
+    "error",
+    {
+      "singleQuote": true,
+      "parser": "flow"
+    }
+  ]
 };
