691431b763bfeacffb62aaa2d8da8d621369ebbc
MelCode <madowl052@gmail.com>
2023-05-28 22:30:02 +0300

    define initial state of FilterSlice

--- client/src/store/reducers/UsersFiltersSlice.js
+++ client/src/store/reducers/UsersFiltersSlice.js
@@ -1,5 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+/*
+
+interface ITextFilter {
+  name: string;
+  text: string;
+  type: 'text';
+  value: string;
+}
+
+interface IChecksItem {
+  label: string;
+  value: string;
+}
+
+interface IChecksFilter {
+  name: string;
+  text: string;
+  type: 'checks';
+  value: IChecksItem[];
+}
+
+interface IRangeFilter {
+  name: string;
+  text: string;
+  type: 'range';
+  min: number;
+  max: number;
+  value: null | [number, number];
+}
+
+initialState: (ITextFilter | IChecksFilter | IRangeFilter)[]
+
+*/
+
 const initialState = [
   {
     name: 'name',
