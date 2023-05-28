0246d4a9e2f9479bbb53292a0cb1a49ee6d63f82
MelCode <madowl052@gmail.com>
2023-05-28 21:48:39 +0300

    fix SearchByRange component

--- client/src/components/SearchPanel/Search/SearchByRange/SearchByRange.js
+++ client/src/components/SearchPanel/Search/SearchByRange/SearchByRange.js
@@ -1,31 +1,32 @@
-import { useState } from 'react'
-
 import { SearchBox } from '../Search.styles'
 
 import { RangeItem, RangeList, StyledRange } from './SearchByRange.styles'
 
-const SearchByRange = ({ currFilter }) => {
-  const [value, setValue] = useState([currFilter.min, currFilter.max])
-
-  console.log(value)
-
+const SearchByRange = ({ currFilter, currFilterIndex, setFilterValue }) => {
   const rangeItems = []
 
   for (let i = 1; i <= currFilter.max; i++) {
     rangeItems.push(<RangeItem key={i}>{i}</RangeItem>)
   }
 
+  const handleChange = (value) => {
+    if (!currFilter.value || currFilter.value[0] !== value[0] || currFilter.value[1] !== value[1]) {
+      console.log(1)
+      setFilterValue(currFilterIndex, value)
+    }
+  }
+
   return (
     <SearchBox gap="24px" padding="8px 30px">
       <StyledRange
         getAriaLabel={() => `${currFilter.name} range`}
-        value={value}
+        value={!currFilter.value ? [currFilter.min, currFilter.max] : currFilter.value}
         sx={{
           padding: 0,
           width: 'calc(100% - 46px)',
           height: '100%',
         }}
-        onChange={(e, newValue) => setValue(newValue)}
+        onChange={(e, newValue) => handleChange(newValue)}
         min={currFilter.min}
         max={currFilter.max}
         // getAriaValueText={valuetext}
