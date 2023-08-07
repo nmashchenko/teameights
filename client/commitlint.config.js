3b88d3f2be1bed450a5cdf98113bea8967149977
Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>
2023-08-06 21:52:26 -0500

    ci: enable commitlint

--- client/commitlint.config.js
+++ client/commitlint.config.js
@@ -1,4 +1,3 @@
-// build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
 // ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
 // docs: Documentation only changes
 // feat: A new feature
