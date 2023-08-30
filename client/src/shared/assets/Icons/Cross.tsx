dccfe98d5583fc64412a5d79a88c675ad96fe768
“merankori” <skiselev@team.amocrm.com>
2023-08-30 21:20:23 +0300

    update props for cross

--- client/src/shared/assets/Icons/Cross.tsx
+++ client/src/shared/assets/Icons/Cross.tsx
@@ -1,19 +1,19 @@
-export const Cross = (props: any) => {
+export const Cross: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-      width="20"
-      height="20"
-      viewBox="0 0 52 52"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='20'
+      height='20'
+      viewBox='0 0 52 52'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
       {...props}
     >
       <path
-        d="M31.625 20.375L20.375 31.625M31.625 31.625L20.375 20.375"
-        stroke="white"
-        stroke-width="1.25"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        d='M31.625 20.375L20.375 31.625M31.625 31.625L20.375 20.375'
+        stroke='white'
+        stroke-width='1.25'
+        stroke-linecap='round'
+        stroke-linejoin='round'
       />
     </svg>
   );
