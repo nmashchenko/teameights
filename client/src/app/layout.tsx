eac12f0bc38532bd0bd16667b4034da9557f8b58
dmtrack <dmtrack.dev@gmail.com>
2023-09-04 13:26:08 +0500

    feat: rquery installed and initialized

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,22 +1,21 @@
 import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
+import { ReactQueryProvider } from 'shared/api/ReactQueryProvider';
 import './styles/globals.scss';
 
 const inter = Rubik({ subsets: ['latin'], variable: '--font-rubik' });
 
 export const metadata: Metadata = {
   title: 'Teameights',
-  description: 'Welcome to Teameights!',
+  description: 'Welcome to Teameights!'
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
-    </html>
+    <ReactQueryProvider>
+      <html lang='en'>
+        <body className={inter.variable}>{children}</body>
+      </html>
+    </ReactQueryProvider>
   );
 }
