660a5fe517dca8867b058d61ab42900fd96bdb24
dmtrack <dmtrack.dev@gmail.com>
2023-09-04 14:01:08 +0500

    chore: rquery version updated to beta-v5

--- client/src/shared/api/ReactQueryProvider.tsx
+++ client/src/shared/api/ReactQueryProvider.tsx
@@ -1,8 +1,18 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query?.meta?.errorMessage) {
+        // after toast will be installed, instead of the string bellow
+        // will be: toast.error(query.meta.errorMessage)
+        console.error(query.meta.errorMessage);
+      }
+    }
+  })
+});
 
 export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
