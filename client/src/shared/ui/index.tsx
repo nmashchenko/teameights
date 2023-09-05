fa15affa18cf9b58d7fc8b42cfc24970a5b58f80
Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>
2023-09-05 03:35:40 -0500

    fix: fixed naming, import

--- client/src/shared/ui/index.tsx
+++ client/src/shared/ui/index.tsx
@@ -3,6 +3,7 @@ export * from './badge/language';
 export * from './button';
 export * from './checkbox';
 export * from './input';
+export * from './select';
 export * from './tabs';
 export * from './textarea';
 export * from './typography';
