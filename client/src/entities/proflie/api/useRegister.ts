8ff7b17ec94f014e2a3b37233dcecebd9718612a
dmtrack <dmtrack.dev@gmail.com>
2023-09-08 21:34:29 +0500

    chore: added axios and config

--- client/src/entities/proflie/api/useRegister.ts
+++ client/src/entities/proflie/api/useRegister.ts
@@ -1,33 +1,33 @@
-import { useRouter } from 'next/router';
-import { http } from 'shared/api';
+// import { useRouter } from 'next/router';
+// import { http } from 'shared/api';
 
-import { errorToaster } from '../../../shared/components/Toasters/Error.toaster';
-import { userAuth } from '../../../store/reducers/UserAuth';
-import { useMutation } from '@tanstack/react-query';
-import { ROUTES } from 'shared/constant/routes';
+// import { errorToaster } from '../../../shared/components/Toasters/Error.toaster';
+// import { userAuth } from '../../../store/reducers/UserAuth';
+// import { useMutation } from '@tanstack/react-query';
+// import { ROUTES } from 'shared/constant/routes';
 
-const { api } = http;
+// const { api } = http;
 
-export const useRegister = () => {
-  const router = useRouter();
-  const registerUser = async (registrationDetails) => {
-    return await api.post('/auth/registration', registrationDetails);
-  };
+// export const useRegister = () => {
+//   const router = useRouter();
+//   const registerUser = async (registrationDetails) => {
+//     return await api.post('/auth/registration', registrationDetails);
+//   };
 
-  return useMutation(registerUser, {
-    mutationKey: 'registerUser',
-    onMutate: () => {
-      // clear previous error before making new request
-      // dispatch(userAuth.actions.authClearError());
-    },
-    onSuccess: (data) => {
-      // save accessToken
-      localStorage.setItem('token', data.data.accessToken);
-      router.push(ROUTES.confirmEmail);
-    },
-    onError: (error: Error) => {
-      // set error message
-      errorToaster(error);
-    }
-  });
-};
+//   return useMutation(registerUser, {
+//     mutationKey: 'registerUser',
+//     onMutate: () => {
+//       // clear previous error before making new request
+//       // dispatch(userAuth.actions.authClearError());
+//     },
+//     onSuccess: (data) => {
+//       // save accessToken
+//       localStorage.setItem('token', data.data.accessToken);
+//       router.push(ROUTES.confirmEmail);
+//     },
+//     onError: (error: Error) => {
+//       // set error message
+//       errorToaster(error);
+//     }
+//   });
+// };
