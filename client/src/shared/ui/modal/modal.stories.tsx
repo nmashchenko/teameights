19cf118d3cc027b62bbea0b02338fb34274036dc
MelancholicCode <96338248+MelancholicCode@users.noreply.github.com>
2023-09-09 12:45:10 +0300

    feat: reusable modal (#55)
    * add Modal component
    
    * fix children placing
    
    * fixes for modal
    
    * add modal on page
    
    * add stories for Modal
    
    * fix formatting
    
    * update yarn lock
    
    * update props for cross
    
    * add animations for modal
    
    * fix: modal story
    
    * feat: size changing support
    
    * fix: deleted old Button folder
    
    * fix: deleted old Fields folder
    
    * fix: deleted old Typography folder
    
    * fix: deleted old Tabs folder
    
    * fix: fixed exports
    
    * fix: close icon
    
    ---------
    
    Co-authored-by: skiselev <skiselev@team.amocrm.com>
    Co-authored-by: Nikita Mashchenko <52038455+exortme1ster@users.noreply.github.com>

---
+++ client/src/shared/ui/modal/modal.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta } from '@storybook/react';
+import { useState } from 'react';
+import { Button } from '../button';
+import { Modal } from './ui';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Modal> = {
+  title: 'shared/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+  argTypes: {}
+};
+
+export default meta;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Modal_Opened = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        Some Text
+      </Modal>
+      <Button typeBtn='primary' size='m' onClick={() => setIsModalOpen(true)}>
+        Open modal
+      </Button>
+    </div>
+  );
+};
