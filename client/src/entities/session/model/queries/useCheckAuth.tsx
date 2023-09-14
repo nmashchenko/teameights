97b6bf854d8094a5b0d32a9d71015656dcb73f44
dmtrack <dmtrack.dev@gmail.com>
2023-09-14 20:03:18 +0500

    feat: useLogin and useCheckAuth are implemented

--- client/src/entities/session/model/queries/useCheckAuth.tsx
+++ client/src/entities/session/model/queries/useCheckAuth.tsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
 import { http } from 'shared/api';
 import { IUserResponse } from '../types';
 import { AxiosResponse } from 'axios';
