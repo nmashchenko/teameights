093cdcd62d6f487dd3dc1c5bd4c1a4a946bf30fd
LanselonX <Lanselon1221@gmail.com>
2023-09-15 20:39:07 +0300

    fix: added animations for the component and updated the button click logic

--- client/src/features/cookie-banner/cookie-banner.tsx
+++ client/src/features/cookie-banner/cookie-banner.tsx
@@ -1,37 +1,67 @@
+import { useState, useRef, useEffect } from 'react';
 import { Typography, Button } from '../../shared/ui';
 import styles from './cookie-banner.module.scss';
 import Image from 'next/image';
 
 export const CookieBanner = () => {
+  const elemRef = useRef<HTMLDivElement | null>(null);
+  const [open, setOpen] = useState(true);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      if (elemRef.current) {
+        elemRef.current.removeEventListener('animationend', () => {
+          setOpen(false);
+        });
+      }
+    };
+  }, []);
+
+  const handleAccept = () => {
+    if (!buttonClicked) {
+      console.log('Кнопка Accept нажата');
+      elemRef.current?.classList.add(styles.closeIt);
+      setButtonClicked(true);
+    }
+  };
+
+  const handleDecline = () => {
+    if (!buttonClicked) {
+      console.log('Кнопка Decline нажата');
+      elemRef.current?.classList.add(styles.closeIt);
+      setButtonClicked(true);
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.inline}>
-        <div>
-          <Image src='/images/cookie.png' width='50' height='50' alt='cookies' />
+    <>
+      {open && (
+        <div ref={elemRef} className={`${styles.container} ${styles.openIt}`}>
+          <div className={styles.inline}>
+            <div>
+              <Image src='/images/cookie.png' width='50' height='50' alt='cookies' />
+            </div>
+            <div>
+              <Typography color='white' size='body_s'>
+                Our website use cookies. By continuing, we assume your permission to deploy cookies
+                as detailed in our{' '}
+                <a href='#' className={styles.privacy}>
+                  Privacy Policy.
+                </a>
+              </Typography>
+            </div>
+          </div>
+          <div className={styles.button}>
+            <Button size='m' typeBtn='primary' width='152px' onClick={handleAccept}>
+              Accept
+            </Button>
+            <Button size='m' typeBtn='secondary' onClick={handleDecline}>
+              Decline
+            </Button>
+          </div>
         </div>
-        <div>
-          <Typography color='white' size='body_s'>
-            Our website use cookies. By continuing, we assume your permission to deploy cookies as
-            detailed in our{' '}
-            <a href='#' className={styles.privacy}>
-              Privacy Policy.
-            </a>
-          </Typography>
-        </div>
-      </div>
-      <div className={styles.button}>
-        <Button
-          size='m'
-          typeBtn='primary'
-          width='152px'
-          onClick={() => console.log('Кнопка Accept нажата')}
-        >
-          Accept
-        </Button>
-        <Button size='m' typeBtn='secondary' onClick={() => console.log('Кнопка Decline нажата')}>
-          Decline
-        </Button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
