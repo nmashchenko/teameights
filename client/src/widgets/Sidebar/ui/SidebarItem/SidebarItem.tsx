b286d1f606fee43167917bb45c5d2a896d826a3d
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-09-23 13:54:13 +0300

    refactor: ♻️ refactor sidebar-item

--- client/src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ client/src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -1,25 +1,21 @@
 import Link from 'next/link';
 import clsx from 'clsx';
+import React from 'react';
+
 import styles from './SidebarItem.module.scss';
 
-type SidebarItemProps = React.LiHTMLAttributes<HTMLLIElement> & {
+interface SidebarItemProps extends React.LiHTMLAttributes<HTMLLIElement> {
   active: boolean;
   path: string;
   icon: React.ReactNode;
   title: string;
   isActive: boolean;
-};
+}
 
-export const SidebarItem: React.FC<SidebarItemProps> = ({
-  active,
-  icon,
-  path,
-  title,
-  isActive,
-  ...props
-}) => {
+export const SidebarItem: React.FC<SidebarItemProps> = props => {
+  const { active, path, icon, title, isActive, ...rest } = props;
   return (
-    <li {...props}>
+    <li {...rest}>
       <Link href={path} className={clsx(styles.wrapper, { [styles.active]: isActive })}>
         <div className={styles.icon}>{icon}</div>
         <span className={clsx(styles.title, { [styles.active]: active })}>{title}</span>
