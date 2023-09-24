45b47ac22daf8dcba30f7f2896dca6e5d152b952
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-09-25 01:47:38 +0300

    docs: :memo: add docs for Flex component

---
+++ client/src/shared/ui/flex/flex.stories.tsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+import { Flex, FlexProps } from './flex';
+
+const flexProps: FlexProps = {
+  align: 'center',
+  justify: 'space-between',
+  gap: 10,
+  margin: 20,
+  padding: 20,
+  direction: 'row',
+  maxHeight: '500px',
+  position: 'relative',
+  width: '100%',
+  maxWidth: '500px',
+  height: 'auto',
+  children: <div>Item 1</div>,
+};
+
+// Defining meta information for Storybook
+type Story = StoryObj<typeof Flex>;
+const FlexTemplate: Story = {
+  render: args => (
+    <Flex {...args}>
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+    </Flex>
+  ),
+};
+
+export const Playground = { ...FlexTemplate };
+Playground.args = flexProps;
+
+// Flex with column direction
+export const ColumnDirection = { ...FlexTemplate };
+ColumnDirection.args = {
+  ...flexProps,
+  direction: 'column',
+};
+
+// Flex with different alignment
+export const DifferentAlignment = { ...FlexTemplate };
+DifferentAlignment.args = {
+  ...flexProps,
+  align: 'flex-start',
+  justify: 'flex-end',
+};
+
+// Flex with different spacing
+export const DifferentSpacing = { ...FlexTemplate };
+DifferentSpacing.args = {
+  ...flexProps,
+  gap: 20,
+  margin: 10,
+  padding: 10,
+};
+
+// Flex with different dimensions
+export const DifferentDimensions = { ...FlexTemplate };
+DifferentDimensions.args = {
+  ...flexProps,
+  width: '50%',
+  maxWidth: '300px',
+  height: '200px',
+};
+
+const flexArgTypes: ArgTypes = {
+  align: {
+    control: {
+      type: 'select',
+    },
+    options: ['flex-start', 'flex-end', 'center', 'baseline', 'stretch', 'normal'],
+    defaultValue: { summary: 'normal' },
+    description: 'Defines the default behavior for how items are laid out along the cross axis.',
+  },
+  justify: {
+    control: {
+      type: 'select',
+    },
+    options: [
+      'flex-start',
+      'flex-end',
+      'center',
+      'space-between',
+      'space-around',
+      'space-evenly',
+      'normal',
+    ],
+    defaultValue: { summary: 'normal' },
+    description:
+      'Defines how the browser distributes space between and around content items along the main axis.',
+  },
+  direction: {
+    control: {
+      type: 'select',
+    },
+    options: ['row', 'column'],
+    defaultValue: { summary: 'row' },
+    description: 'Defines in which direction the container wants to stack the flex items.',
+  },
+  position: {
+    control: {
+      type: 'select',
+    },
+    options: ['static', 'relative', 'absolute', 'fixed', 'sticky'],
+    defaultValue: { summary: 'static' },
+    description: 'Specifies the type of positioning method used for an element.',
+  },
+  gap: {
+    control: 'text',
+    defaultValue: { summary: '0' },
+    description: 'Defines the space between flex items.',
+  },
+  margin: {
+    control: 'text',
+    defaultValue: { summary: '0' },
+    description: 'Defines the space around the component.',
+  },
+  padding: {
+    control: 'text',
+    defaultValue: { summary: '0' },
+    description: 'Defines the space inside the component.',
+  },
+  maxHeight: {
+    control: 'text',
+    defaultValue: { summary: 'none' },
+    description: 'Defines the maximum height of the component.',
+  },
+  width: {
+    control: 'text',
+    defaultValue: { summary: 'auto' },
+    description: 'Defines the width of the component.',
+  },
+  maxWidth: {
+    control: 'text',
+    defaultValue: { summary: 'none' },
+    description: 'Defines the maximum width of the component.',
+  },
+  height: {
+    control: 'text',
+    defaultValue: { summary: 'auto' },
+    description: 'Defines the height of the component.',
+  },
+};
+
+export default {
+  title: 'shared/Flex',
+  component: Flex,
+  tags: ['autodocs'],
+  argTypes: flexArgTypes,
+} as Meta;
--- client/src/shared/ui/flex/flex.tsx
+++ client/src/shared/ui/flex/flex.tsx
@@ -2,21 +2,82 @@ import React from 'react';
 
 import styles from './flex.module.scss';
 
-interface FlexProps {
+export interface FlexProps {
+  /**
+   * [props.align] - The align-items property defines the default behavior for how items are laid out along the cross axis (perpendicular to the main axis). Default is 'normal'.
+   */
   align?: string;
+  /**
+   * [props.justify] - The justify-content property defines how the browser distributes space between and around content items along the main axis of their container. Default is 'normal'.
+   */
   justify?: string;
+  /**
+   * [props.gap] - The gap property defines the space between flex items. Default is 0.
+   */
   gap?: string | number;
+  /**
+   * [props.margin] - The margin property defines the space around the component. Default is 0.
+   */
   margin?: string | number;
+  /**
+   * [props.padding] - The padding property defines the space inside the component. Default is 0.
+   */
   padding?: string | number;
+  /**
+   * [props.direction] - The flex-direction property defines in which direction the container wants to stack the flex items. Default is 'row'.
+   */
   direction?: 'row' | 'column';
+  /**
+   * [props.maxHeight] - The max-height property defines the maximum height of the component. Default is 'none'.
+   */
   maxHeight?: string;
+  /**
+   * [props.position] - The position property specifies the type of positioning method used for an element. Default is 'static'.
+   */
   position?: string;
+  /**
+   * [props.width] - The width property defines the width of the component. Default is 'auto'.
+   */
   width?: string;
+  /**
+   * [props.maxWidth] - The max-width property defines the maximum width of the component. Default is 'none'.
+   */
   maxWidth?: string;
+  /**
+   * [props.height] - The height property defines the height of the component. Default is 'auto'.
+   */
   height?: string;
+  /**
+   * props.children - The content to be laid out.
+   */
   children: React.ReactNode;
 }
 
+/**
+ * The `Flex` component is a flexible box layout component that provides a more efficient way to lay out, align, and distribute space among items in a container, even when their sizes are unknown or dynamic.
+ *
+ * Example usage:
+ *
+ * ```tsx
+ * <Flex
+ *   align="center"
+ *   justify="space-between"
+ *   gap={10}
+ *   margin={20}
+ *   padding={20}
+ *   direction="row"
+ *   maxHeight="500px"
+ *   position="relative"
+ *   width="100%"
+ *   maxWidth="500px"
+ *   height="auto"
+ * >
+ *   <div>Item 1</div>
+ *   <div>Item 2</div>
+ *   <div>Item 3</div>
+ * </Flex>
+ * ```
+ */
 export const Flex: React.FC<FlexProps> = props => {
   const {
     align,
