3fbb6060bd5b517243a80dc827f6abfa402aafa1
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-09-25 01:02:06 +0300

    feat: :sparkles: add flex component

---
+++ client/src/shared/ui/flex/flex.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import styles from './flex.module.scss';
+
+interface FlexProps {
+  align?: string;
+  justify?: string;
+  gap?: string | number;
+  margin?: string | number;
+  padding?: string | number;
+  direction?: 'row' | 'column';
+  maxHeight?: string;
+  position?: string;
+  width?: string;
+  maxWidth?: string;
+  height?: string;
+  children: React.ReactNode;
+}
+
+export const Flex: React.FC<FlexProps> = props => {
+  const {
+    align,
+    justify,
+    gap,
+    margin,
+    padding,
+    direction,
+    maxHeight,
+    position,
+    width,
+    maxWidth,
+    height,
+    children,
+  } = props;
+
+  const style = {
+    '--align': align,
+    '--justify': justify,
+    '--gap': gap,
+    '--margin': margin,
+    '--padding': padding,
+    '--direction': direction,
+    '--maxHeight': maxHeight,
+    '--position': position,
+    '--width': width,
+    '--maxWidth': maxWidth,
+    '--height': height,
+  } as React.CSSProperties;
+
+  return (
+    <div className={styles.flexWrapper} style={style}>
+      {children}
+    </div>
+  );
+};
