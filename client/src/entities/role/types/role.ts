78f8de249012b69562f23a69411fa193d4502cc1
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-09-25 18:08:49 +0300

    feat: ✨ add types for user, team, role, notification

---
+++ client/src/entities/role/types/role.ts
@@ -0,0 +1,7 @@
+export interface Role {
+  _id: string;
+  value: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ client/src/entities/user/types/user.ts
@@ -0,0 +1,49 @@
+import { Notification } from '@/entities/notification';
+import { Role } from '@/entities/role';
+import { Team } from '@/entities/team';
+
+export interface User {
+  _id: string;
+  email: string;
+  password: string;
+  username: string;
+  fullName: string;
+  isActivated?: boolean;
+  isRegistered?: boolean;
+  isLeader?: boolean;
+  activationLink?: string;
+  country?: string;
+  dateOfBirth?: Date;
+  universityData?: {
+    university: string;
+    degree: string;
+    major: string;
+    addmissionDate: Date;
+    graduationDate: Date;
+  }[];
+  jobData?: {
+    title: string;
+    company: string;
+    startDate: Date;
+    endDate: Date;
+  }[];
+  projectData?: {
+    title: string;
+    link: string;
+  }[];
+  concentration?: string;
+  description?: string;
+  experience?: string;
+  image: string;
+  links?: {
+    github?: string;
+    linkedIn?: string;
+    behance?: string;
+    telegram?: string;
+  };
+  programmingLanguages?: string[];
+  frameworks?: string[];
+  roles?: Role[]; // Assuming Role is represented by its ID on the frontend
+  notifications?: Notification[]; // Assuming Notification is represented by its ID on the frontend
+  team?: Team; // Assuming Team is represented by its ID on the frontend
+}
