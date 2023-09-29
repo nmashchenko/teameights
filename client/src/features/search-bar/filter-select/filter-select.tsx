66025c611ed1270bf3e213b413fb518bb65ba2f8
“merankori” <skiselev@team.amocrm.com>
2023-09-30 02:33:45 +0300

    fix: some fixes for select

--- client/src/features/search-bar/filter-select/filter-select.tsx
+++ client/src/features/search-bar/filter-select/filter-select.tsx
@@ -1,15 +1,14 @@
 import clsx from 'clsx';
 import { FC, useState } from 'react';
-import { Controller } from 'react-hook-form';
+import { useFormContext } from 'react-hook-form';
 import { Select } from 'shared/ui';
-import { Filter } from '../interface/filters';
 import styles from './filter-select.module.scss';
 
-interface IFilterSelectProps {
-  filtersArr: Filter[];
-}
+interface IFilterSelectProps {}
 
-const FilterSelect: FC<IFilterSelectProps> = ({ filtersArr }) => {
+const FilterSelect: FC<IFilterSelectProps> = () => {
+  const { getValues, setValue } = useFormContext();
+
   const [isMenuHovered, setIsMenuHovered] = useState<boolean>(false);
   const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
 
@@ -18,31 +17,30 @@ const FilterSelect: FC<IFilterSelectProps> = ({ filtersArr }) => {
     setIsMenuHovered(false);
   };
 
+  const handleChange = newValue => {
+    const filterIndex = getValues('filtersArr').findIndex(item => item.value === newValue.value);
+    setValue('currentFilterIndex', filterIndex);
+  };
+
   return (
-    <Controller
-      defaultValue={filtersArr[0]}
-      name='currentFilter'
-      render={({ field: { onChange, value: newValue } }) => (
-        <div
-          className={clsx(styles.selectWrapper)}
-          onMouseEnter={() => setIsMenuHovered(true)}
-          onMouseLeave={() => setIsMenuHovered(false)}
-        >
-          <Select
-            options={filtersArr}
-            isWithBorder={false}
-            classNames={{
-              container: () =>
-                isMenuHovered || isMenuOpened ? clsx(styles.select_active) : clsx(styles.select),
-            }}
-            value={newValue}
-            onChange={value => onChange(value)}
-            onMenuOpen={() => setIsMenuOpened(true)}
-            onMenuClose={menuCloseHandler}
-          />
-        </div>
-      )}
-    />
+    <div
+      className={clsx(styles.selectWrapper)}
+      onMouseEnter={() => setIsMenuHovered(true)}
+      onMouseLeave={() => setIsMenuHovered(false)}
+    >
+      <Select
+        options={getValues('filtersArr')}
+        isWithBorder={false}
+        classNames={{
+          container: () =>
+            isMenuHovered || isMenuOpened ? clsx(styles.select_active) : clsx(styles.select),
+        }}
+        defaultValue={getValues('filtersArr')[getValues('currentFilterIndex')]}
+        onChange={handleChange}
+        onMenuOpen={() => setIsMenuOpened(true)}
+        onMenuClose={menuCloseHandler}
+      />
+    </div>
   );
 };
 
