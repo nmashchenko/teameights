fbebf9bd4827b93e2d640322ad4ebe9708a72db0
“merankori” <skiselev@team.amocrm.com>
2023-09-30 02:35:29 +0300

    feat: add draft search-input functionality

--- client/src/features/search-bar/search-bar.tsx
+++ client/src/features/search-bar/search-bar.tsx
@@ -4,6 +4,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import FilterSelect from './filter-select/filter-select';
 import { Filter } from './interface/filters';
 import styles from './search-bar.module.scss';
+import SearchInput from './search-input/search-input';
 
 const mockFiltersArr: Filter[] = [
   {
@@ -16,24 +17,33 @@ const mockFiltersArr: Filter[] = [
     label: 'Countries',
     value: 'countries',
     type: 'checkbox',
+    optionsArr: [
+      { label: 'Japan', value: 'jp' },
+      { label: 'Russia', value: 'ru' },
+      { label: 'Ukraine', value: 'ua' },
+      { label: 'Korea', value: 'kr' },
+    ],
     filterValue: [],
   },
   {
     label: 'Languages',
     value: 'languages',
     type: 'checkbox',
+    optionsArr: [],
     filterValue: [],
   },
   {
     label: 'Frameworks',
     value: 'frameworks',
     type: 'checkbox',
+    optionsArr: [],
     filterValue: [],
   },
   {
     label: 'Concentrations',
     value: 'concentrations',
     type: 'checkbox',
+    optionsArr: [],
     filterValue: [],
   },
 ];
@@ -43,14 +53,19 @@ interface ISearchBarProps {
 }
 
 const SearchBar: FC<ISearchBarProps> = ({ filtersArr = mockFiltersArr }) => {
-  const methods = useForm();
+  const methods = useForm({
+    defaultValues: {
+      currentFilterIndex: 0,
+      filtersArr: filtersArr,
+    },
+  });
 
   return (
     <FormProvider {...methods}>
       <div className={clsx(styles.searchBar)}>
         <div className={clsx(styles.searchBarContent)}>
-          <FilterSelect filtersArr={filtersArr} />
-          {/* Search */}
+          <FilterSelect />
+          <SearchInput />
         </div>
         {/* TagsList */}
       </div>
---
+++ client/src/features/search-bar/search-input/search-input.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Input, Select } from 'shared/ui';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
+
+const SearchInput = () => {
+  const { register, getValues } = useFormContext();
+  const filterIndex = useWatch({
+    name: 'currentFilterIndex',
+  });
+
+  const currentFilter = getValues(`filtersArr.${filterIndex}`);
+
+  console.log(currentFilter);
+
+  switch (currentFilter.type) {
+    case 'text':
+      return <Input {...register(`filtersArr.${filterIndex}.filterValue`)} isWithBorder={false} />;
+
+    case 'checkbox':
+      return (
+        <Controller
+          name={`filtersArr.${filterIndex}.filterValue`}
+          render={({ field: { onChange, value } }) => (
+            <Select
+              onChange={value => onChange(value)}
+              value={value}
+              options={getValues(`filtersArr.${filterIndex}.optionsArr`)}
+              isWithBorder={false}
+              isCheckbox
+              isMulti
+            />
+          )}
+        />
+      );
+
+    default:
+  }
+};
+
+export default SearchInput;
