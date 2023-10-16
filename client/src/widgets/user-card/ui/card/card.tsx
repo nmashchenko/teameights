8db2df8179bdf6aa86578aaef7aec57ea73961c9
Berezhnev Vladimir <vova21473@gmail.com>
2023-10-16 20:36:56 +1000

    fix(user-card): fix placement of elements

--- client/src/widgets/user-card/ui/card/card.tsx
+++ client/src/widgets/user-card/ui/card/card.tsx
@@ -15,22 +15,6 @@ export const UserCard: FC<UserCardProps> = props => {
   const programmingLanguagesAmount = programmingLanguages.length;
   const frameworksAmount = frameworks.length;
 
-  const renderBadges = (
-    data: string[],
-    BadgeComponent: typeof BadgeLanguage | typeof BadgeFramework,
-    andMore: boolean = false,
-    andMoreAmount: number = 0
-  ) => {
-    const sliceAmount = andMore ? 1 : 2;
-    const mappedData = data.slice(0, sliceAmount);
-
-    return mappedData.map((item, id) => {
-      const key = andMore ? 0 : id;
-      const props = andMore ? { isAndMore: true, andMoreAmount } : { data: item };
-      return <BadgeComponent key={key} {...props} />;
-    });
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -39,17 +23,20 @@ export const UserCard: FC<UserCardProps> = props => {
             <Image className={styles.user_logo} src={image} alt='' width={70} height={70} />
             <div className={styles.languagesContainer}>
               {programmingLanguagesAmount <= 2 ? (
-                renderBadges(programmingLanguages, BadgeLanguage)
+                programmingLanguages
+                  .slice(0, programmingLanguagesAmount < 2 ? programmingLanguagesAmount : 2)
+                  .map((item, id) => <BadgeLanguage data={item} key={id} />)
+              ) : programmingLanguagesAmount > 2 ? (
+                programmingLanguages
+                  .slice(0, programmingLanguagesAmount < 2 ? programmingLanguagesAmount : 1)
+                  .map((item, id) => <BadgeLanguage data={item} key={id} />)
               ) : (
-                <>
-                  {renderBadges(programmingLanguages, BadgeLanguage)}
-                  {renderBadges(
-                    programmingLanguages,
-                    BadgeLanguage,
-                    true,
-                    programmingLanguagesAmount - 1
-                  )}
-                </>
+                <div></div>
+              )}
+              {programmingLanguagesAmount > 2 ? (
+                <BadgeLanguage isAndMore={true} andMoreAmount={programmingLanguagesAmount - 1} />
+              ) : (
+                ''
               )}
             </div>
           </div>
@@ -63,12 +50,25 @@ export const UserCard: FC<UserCardProps> = props => {
         </div>
 
         <div className={styles.frameworksContainer}>
-          {renderBadges(
-            frameworks,
-            BadgeFramework,
-            false,
-            programmingLanguagesAmount < 5 ? programmingLanguagesAmount : 3
-          )}
+          {frameworks
+            .slice(0, programmingLanguagesAmount < 5 ? programmingLanguagesAmount : 3)
+            .map((item, id) => (
+              <>
+                {frameworksAmount < 5 ? (
+                  <>
+                    <div className={styles.framework}>
+                      <BadgeFramework data={item} key={id} />
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className={styles.framework}>
+                      <BadgeFramework data={item} key={id} />
+                    </div>
+                  </>
+                )}
+              </>
+            ))}
           {frameworksAmount > 4 && (
             <BadgeFramework isAndMore={true} andMoreAmount={frameworksAmount - 1} />
           )}
