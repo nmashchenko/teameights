dcaf714fbea25a22f33fac540187be4c4ead7a07
LanselonX <Lanselon1221@gmail.com>
2023-10-18 22:54:56 +0300

    Revert "fix: added storybook for modal"
    This reverts commit c5e7773c4e5cf6d25ef83ddb866da5fa20a6f4d6.

---
+++ client/src/widgets/modals/interactive-modal/interactive-modal.tsx
@@ -0,0 +1,33 @@
+import { Flex, Modal, Typography } from '@/shared/ui';
+import { SelectAutocomplete } from '@/shared/ui/select/ui/select-autocomplete/select-autocomplete';
+import { FC, PropsWithChildren } from 'react';
+import styles from './ineractive-modal.module.scss';
+
+export interface InteractiveModalProps {
+  heading: string;
+  sub?: string;
+}
+
+export const InteractiveModal: FC<PropsWithChildren<InteractiveModalProps>> = ({
+  heading,
+  sub,
+  children,
+}) => {
+  return (
+    <div>
+      <Modal isOpen={true} size='s'>
+        <Flex height='350px' direction='column' gap='24px'>
+          <div className={styles.text}>
+            <Typography color='white' size='heading_m'>
+              {heading}
+            </Typography>
+          </div>
+          <div>
+            <SelectAutocomplete name='concentration' />
+          </div>
+        </Flex>
+        <Flex>{children}</Flex>
+      </Modal>
+    </div>
+  );
+};
