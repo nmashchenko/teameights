9d85fc2b7896f7a6f7ba42fdec474b336f8a7f1e
LanselonX <Lanselon1221@gmail.com>
2023-11-24 06:01:46 +0200

    feat: clean up the icons

--- client/src/shared/assets/icons/x.tsx
+++ client/src/shared/assets/icons/x.tsx
@@ -1,14 +1,15 @@
-import { FC, SVGProps } from 'react';
+import { FC } from 'react';
+import { SVGPropsWithSize } from '@/shared/types/svg-props-with-size';
 
-export const X: FC<SVGProps<SVGSVGElement>> = props => {
+export const X: FC<SVGPropsWithSize> = ({ size = '20', ...rest }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='20'
-      height='20'
+      width={size}
+      height={size}
       fill='#ffffff'
       viewBox='0 0 256 256'
-      {...props}
+      {...rest}
     >
       <path d='M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z'></path>
     </svg>
