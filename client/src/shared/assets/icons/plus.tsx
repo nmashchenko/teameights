cea84b2d52a56047632bf58b281c184cc4506b40
LanselonX <Lanselon1221@gmail.com>
2023-12-04 10:32:00 +0200

    fix: cleaned up the icons on the new branch

--- client/src/shared/assets/icons/plus.tsx
+++ client/src/shared/assets/icons/plus.tsx
@@ -1,13 +1,14 @@
-import { FC, SVGProps } from 'react';
-export const Plus: FC<SVGProps<SVGSVGElement>> = props => {
+import { SVGPropsWithSize } from '@/shared/types/svg-props-with-size';
+import { FC } from 'react';
+export const Plus: FC<SVGPropsWithSize> = ({ size = '20', ...rest }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='20'
-      height='20'
+      width={size}
+      height={size}
       fill='#ffffff'
       viewBox='0 0 256 256'
-      {...props}
+      {...rest}
     >
       <path d='M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z'></path>
     </svg>
--- client/src/shared/assets/icons/user-plus.tsx
+++ client/src/shared/assets/icons/user-plus.tsx
@@ -1,14 +1,15 @@
-import { FC, SVGProps } from 'react';
+import { SVGPropsWithSize } from '@/shared/types/svg-props-with-size';
+import { FC } from 'react';
 
-export const UserPlus: FC<SVGProps<SVGSVGElement>> = props => {
+export const UserPlus: FC<SVGPropsWithSize> = ({ size = '20', ...rest }) => {
   return (
     <svg
-      width='20'
-      height='20'
+      width={size}
+      height={size}
       viewBox='0 0 20 20'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
-      {...props}
+      {...rest}
     >
       <path
         fillRule='evenodd'
