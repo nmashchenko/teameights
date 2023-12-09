35e90ca9840c98f3bc50d5db48f9016063837a67
LanselonX <Lanselon1221@gmail.com>
2023-12-09 17:24:22 +0200

    fix: added new step

--- client/src/app/onboarding/page.tsx
+++ client/src/app/onboarding/page.tsx
@@ -4,9 +4,7 @@ import { Flex } from '@/shared/ui';
 import { ProgressSection } from '@/app/onboarding/ui/progress-section/progress-section';
 import { ActionSection } from '@/app/onboarding/ui/action-section/action-section';
 import styles from './onboarding.module.scss';
-import { PersonalInfo } from './ui/steps/personal-info/personal-info';
 import { SocialLinks } from './ui/steps/social-links/social-links';
-// import { AccountType } from './ui/steps/accout-type/accout-type';
 const OnboardingPage = () => {
   return (
     <Flex width={'100vw'} height={'100dvh'}>
