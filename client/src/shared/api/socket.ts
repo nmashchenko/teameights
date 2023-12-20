d7e8271ac0bf06bd82bff0d63b7df94d7de0e0e8
Nikita Mashchenko <mashchenko1@icloud.com>
2023-12-19 23:51:53 -0600

    fix: socket issue

--- client/src/shared/api/socket.ts
+++ client/src/shared/api/socket.ts
@@ -1,3 +1,19 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
-export const socket = io('ws://localhost:3001');
+class SocketSingleton {
+  private static instance: Socket | null = null;
+
+  private constructor() {
+    // Private constructor to prevent instantiation
+  }
+
+  public static getInstance(): Socket {
+    if (!SocketSingleton.instance) {
+      // Create a new socket instance if it doesn't exist
+      SocketSingleton.instance = io('ws://localhost:3001');
+    }
+    return SocketSingleton.instance;
+  }
+}
+
+export const socket = SocketSingleton.getInstance();
