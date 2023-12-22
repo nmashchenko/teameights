50f11b1158231472a0d2ae4e00a048aa8ea6b79d
Nikita Mashchenko <mashchenko1@icloud.com>
2023-12-22 02:05:17 -0600

    feat: added initial screen with issues

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -1,22 +1,63 @@
 'use client';
-
-import { Typography } from '@/shared/ui';
+import { Flex, SearchBar } from '@/shared/ui';
 import { useGetScreenWidth } from '@/shared/lib';
-
+import { countries, specialities } from '@/shared/constant';
+import { LogoBig } from '@/shared/assets';
+import { useGetUsers } from '@/entities/session';
+import { useState } from 'react';
+import { Cards } from '@/app/(main)/ui/cards/cards';
+import styles from './layout.module.scss';
 export default function Home() {
   const width = useGetScreenWidth();
+  const [filters, setFilters] = useState<string | null>();
+  const { data: users, isPending } = useGetUsers({
+    page: 1,
+    limit: 50,
+    filters: filters,
+    sort: '',
+  });
 
   return (
     <>
-      <Typography size='heading_l' variant='h6'>
-        We are working hard to deliver teameights on NextJS/TS soon!
-      </Typography>
-
-      <div> The screen width is: {width} </div>
-
-      <a href='/login' style={{ color: 'green' }}>
-        Get to login
-      </a>
+      <Flex
+        gap={48}
+        direction='column'
+        width='100%'
+        justify='center'
+        align='center'
+        className={styles.content_zone}
+      >
+        <LogoBig />
+        <SearchBar
+          initialFiltersState={[
+            {
+              type: 'text',
+              label: 'Name',
+              value: 'name',
+              placeholder: 'Search by name',
+              filterValue: '',
+            },
+            {
+              label: 'Countries',
+              value: 'countries',
+              type: 'checkbox',
+              placeholder: 'Search by countries',
+              optionsArr: countries,
+              filterValue: [],
+            },
+            {
+              label: 'Specialty',
+              value: 'specialty',
+              type: 'multiple',
+              placeholder: 'Search by specialty',
+              optionsArr: specialities,
+              filterValue: [],
+            },
+          ]}
+          onChange={filterValues => console.log(filterValues)}
+        />
+      </Flex>
+      <Cards users={users} isLoading={isPending} />
     </>
   );
 }
