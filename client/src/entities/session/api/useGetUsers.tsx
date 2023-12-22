83f61523ff87965f429ffa168300d383ca8e7670
Nikita Mashchenko <mashchenko1@icloud.com>
2023-12-22 01:59:05 -0600

    feat: added initial implementation

--- client/src/entities/session/api/useGetUsers.tsx
+++ client/src/entities/session/api/useGetUsers.tsx
@@ -1,6 +1,10 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
-import { IUserProtectedResponse } from '@teameights/types';
+import {
+  InfinityPaginationResultType,
+  IUserProtectedResponse,
+  IUserResponse,
+} from '@teameights/types';
 import { API } from '@/shared/api';
 import { API_USERS } from '@/shared/constant';
 
@@ -12,17 +16,17 @@ interface IQueryParams {
 }
 
 export const useGetUsers = ({ page, limit, filters, sort }: IQueryParams) => {
-  const queryString = [
-    page && `page=${page}`,
-    limit && `limit=${limit}`,
-    filters && `filters=${filters}`,
-    sort && `sort=${sort}`,
-  ].join('');
+  // const queryString = [
+  //   page && `page=${page}`,
+  //   limit && `limit=${limit}`,
+  //   filters && `filters=${filters}`,
+  //   sort && `sort=${sort}`,
+  // ].join('');
 
   return useQuery({
-    queryKey: ['useGetUsers', queryString],
+    queryKey: ['useGetUsers'],
     queryFn: async () => {
-      const { data } = await API.get<IUserProtectedResponse>(`${API_USERS}?${queryString}`);
+      const { data } = await API.get<InfinityPaginationResultType<IUserResponse>>(`${API_USERS}`);
       return data;
     },
     refetchOnMount: false,
