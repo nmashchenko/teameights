bf712f5447ad2d276c3c577f31c148f77da7daa0
Berezhnev Vladimir <vova21473@gmail.com>
2024-01-13 01:32:39 +1000

    fix(scss): ChatHeader.tsx

--- client/src/app/(main)/chat/ui/ChatHeader/ChatHeader.tsx
+++ client/src/app/(main)/chat/ui/ChatHeader/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { Typography } from '@/shared/ui';
 import Image from 'next/image';
 import { SearchIcon } from '@/shared/assets'; // TODO: Add another icons: Headphones and ThreeDotsVertilcal
+import styles from './ChatHeader.module.scss';
 
 interface IChatHeader {
   avatarGroup: string;
@@ -10,28 +11,9 @@ interface IChatHeader {
 
 export const ChatHeader = () => {
   return (
-    <div
-      style={{
-        position: 'sticky',
-        width: '100%',
-        height: '90px',
-        borderBottom: '1px solid rgba(67, 71, 82, 1)',
-        display: 'flex',
-        justifyContent: 'space-between',
-      }}
-    >
-      <div style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '100%',
-        margin: '24px 32px 16px 32px',
-      }}>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
+    <div className={styles.container}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.groupInfo}>
           <Image
             style={{ borderRadius: '100px', marginRight: '16px' }}
             src='https://images.unsplash.com/photo-1666597107756-ef489e9f1f09?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -40,16 +22,16 @@ export const ChatHeader = () => {
             height={50}
           />
 
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div className={styles.titlesContainer}>
             <Typography size='heading_s'>Cool developers</Typography>
             <p style={{ color: 'rgba(143, 144, 148, 1)', fontSize: '13px' }}>7 members</p>
           </div>
         </div>
 
-        <div style={{ display: 'flex', gap: '4px', margin: '11px 0px' }}>
-          <SearchIcon size="24" />
-          <SearchIcon size="24" />
-          <SearchIcon size="24" />
+        <div className={styles.icons}>
+          <SearchIcon size='24' />
+          <SearchIcon size='24' />
+          <SearchIcon size='24' />
         </div>
       </div>
     </div>
